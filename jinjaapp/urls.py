bdbb77b167849604472f42a9bfe002282759ed83
jeanclare <jeankarani@gmail.com>
2024-11-08 17:26:56 +0300

    Additional Django

--- jinjaapp/urls.py
+++ jinjaapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from jinjaapp import views
 
 urlpatterns=[
+    path ('',views.home,name='home'),
+    path ('about/',views.about,name='about'),
+    path ('contact/',views.contact,name='contact'),
+    path ('services/',views.services,name='services'),
 
 
 ]
